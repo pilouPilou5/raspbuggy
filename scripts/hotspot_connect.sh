1dbafe5772e55aae507b10b7b9ccd8adeab502e5
Alex <alex.baudry001@gmail.com>
2024-09-11 14:32:00 +0200

    hotspot scripts v2

--- scripts/hotspot_connect.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-nmcli device connect wlan0
--- scripts/hotspot_delete.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo nmcli connection
-echo "Type the name of the hotspot you wish to delete (under NAME) "
-read HOTSPOT_NAME
-sudo nmcli connection delete $HOTSPOT_NAME
