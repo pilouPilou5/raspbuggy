a2f635baf86b506fa84cdb9e9ac892e9f73dbbbc
Paul <paul.jallais@ecole.ensicaen.fr>
2024-09-27 14:24:30 +0200

    gstreamer

--- app_windows/app_windows/app_windows.cpp
+++ app_windows/app_windows/app_windows.cpp
@@ -5,14 +5,77 @@
 #include <Windows.h>
 #include <iostream>
 #include <Xinput.h>
+#include <gst/gst.h>
 
 #pragma comment(lib, "Ws2_32.lib")
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!\n";
+#define IP_ADDRESS "10.42.0.73"  // Replace with your IP address
+#define PORT "5000"                // Replace with your port number
+
+void gst_thread_receive(void) {
+
+    GstElement* pipeline;
+    GstBus* bus;
+    GstMessage* msg;
+    GstStateChangeReturn ret;
+    char command[512];
+
+
+    // Construct the GStreamer pipeline command with specific IP and port
+    snprintf(command, sizeof(command),
+        "udpsrc port=%s caps=\"application/x-rtp, media=(string)video, encoding-name=(string)JPEG\" ! "
+        "rtpjpegdepay ! jpegdec ! videoconvert ! autovideosink",
+        PORT);
+
+    //Create pipeline
+    pipeline = gst_parse_launch(command, NULL);
+    if (!pipeline) {
+        g_printerr("Failed to create pipeline.\n");
+    }
+
+    // Set the pipeline to the PLAYING state
+    ret = gst_element_set_state(pipeline, GST_STATE_PLAYING);
+    if (ret == GST_STATE_CHANGE_FAILURE) {
+        g_printerr("Failed to set pipeline to playing state.\n");
+        gst_object_unref(pipeline);
+    }
+
+    // Get the bus and wait for EOS or ERROR message
+    bus = gst_element_get_bus(pipeline);
+    msg = gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE, (GstMessageType)(GST_MESSAGE_ERROR | GST_MESSAGE_EOS));
+
+    // Handle the received message
+    if (msg != NULL) {
+        if (GST_MESSAGE_TYPE(msg) == GST_MESSAGE_ERROR) {
+            GError* err;
+            gchar* debug_info;
+            gst_message_parse_error(msg, &err, &debug_info);
+            g_printerr("Error: %s\n", err->message);
+            g_free(debug_info);
+            g_error_free(err);
+        }
+        else if (GST_MESSAGE_TYPE(msg) == GST_MESSAGE_EOS) {
+            g_print("End of stream reached.\n");
+        }
+        gst_message_unref(msg);
+    }
+
+    // Clean up
+    gst_object_unref(bus);
+    gst_element_set_state(pipeline, GST_STATE_NULL);
+    gst_object_unref(pipeline);
+}
+
+DWORD WINAPI gstreamerThread(void* p) {
+    gst_thread_receive();
+    return 0;
+}
+
+int main(int argc, char** argv){
+    gst_init(&argc, &argv);
+    HANDLE thread = CreateThread(NULL, 0, gstreamerThread, NULL, 0, NULL);
 
     //initialize winsock
     WSADATA wsaData;
@@ -70,6 +133,6 @@ int main()
                 return 1;
             }
         }
-        Sleep(100);
+        Sleep(20);
     }
 }
